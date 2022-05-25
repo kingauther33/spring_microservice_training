029f6c289178f43735ea30e7eed8b8d1097ccde3
kingauther33 <kingauther33@gmail.com>
2022-05-25 09:56:36 +0700

    Update

---
+++ andinhservices/customer/src/main/java/com/andinh/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.andinh.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ andinhservices/fraud/src/main/java/com/andinh/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.andinh.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
